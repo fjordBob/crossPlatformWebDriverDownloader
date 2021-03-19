5086dcd648dc38d9589c8d301f28fdf782f8826b
daniel kupczyk <notset>
2021-03-19 09:33:05 +0100

    First draft
    Only covers windows.

---
+++ crossPlatformWebDriverDownloader/ChromeWebDriver/ChromeVersion.cs
@@ -0,0 +1,40 @@
+﻿namespace crossPlatformWebDriverDownloader.ChromeWebDriver
+{
+    internal class ChromeVersion
+    {
+        public int Major { get; private set; }
+        public int Minor { get; private set; }
+        public int Build { get; private set; }
+        public int Patch { get; private set; }
+
+        public ChromeVersion(string version)
+        {
+            if (!string.IsNullOrEmpty(version))
+            {
+                string[] versionIdentifiers = version.Split(".");
+
+                //ToDo: Error Handling
+
+                Major = int.Parse(versionIdentifiers[0]);
+                Minor = int.Parse(versionIdentifiers[1]);
+                Build = int.Parse(versionIdentifiers[2]);
+                Patch = int.Parse(versionIdentifiers[3]);
+            }
+        }
+
+        public string GetVersionExcludingPatchVersion()
+        {
+            return string.Format("{0}.{1}.{2}", Major, Minor, Build);
+        }
+
+        public bool EqualsExcludingPatchVersion(ChromeVersion otherVersion)
+        {
+            return (Major == otherVersion.Major && Minor == otherVersion.Minor && Build == otherVersion.Build);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}.{1}.{2}.{3}", Major, Minor, Build, Patch);
+        }
+    }
+}
---
+++ crossPlatformWebDriverDownloader/ChromeWebDriver/ChromeWebDriverInstaller.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace crossPlatformWebDriverDownloader.ChromeWebDriver
+{
+    class ChromeWebDriverInstaller
+    {
+    }
+}
